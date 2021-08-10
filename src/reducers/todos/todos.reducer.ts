3a24794de27148bb3174fa44879f33b0db5e9ca6
Prasetya Aji Baskara <prasetya.baskara@btpn.com>
2021-08-10 10:45:21 +0700

    todos reducer type

--- src/reducers/todos/todos.reducer.ts
+++ src/reducers/todos/todos.reducer.ts
@@ -3,13 +3,15 @@ import { todosTypes } from './todos.types';
 type actionType = {
   type: string,
   payload: string
-}
+};
+
+type todosState = string[];
 
 export const initialState: string[] = [];
 export const initializer = (initialValue = initialState) =>
   JSON.parse(localStorage.getItem("localTodos") || '[]') || initialValue;
 
-export const TodosReducer = (state: string[], action: actionType) => {
+export const TodosReducer = (state: todosState, action: actionType): todosState => {
   switch (action.type) {
     case todosTypes.ADD_TODO:
       console.log('state', state);
