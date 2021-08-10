ef39a5f0feca3ac178733223ac1726422f271ed3
Prasetya Aji Baskara <prasetya.baskara@btpn.com>
2021-08-10 10:47:26 +0700

    todos actions type

--- src/reducers/todos/todos.actions.ts
+++ src/reducers/todos/todos.actions.ts
@@ -1,11 +1,16 @@
 import { todosTypes } from "./todos.types"
 
-export const addTodoAction = (payload: string) => ({
+interface todoActionResult {
+  type: string;
+  payload: string;
+}
+
+export const addTodoAction = (payload: string): todoActionResult => ({
   type: todosTypes.ADD_TODO,
   payload
 });
 
-export const removeTodoAction = (payload: string) => ({
+export const removeTodoAction = (payload: string): todoActionResult => ({
   type: todosTypes.REMOVE_TODO,
   payload
 });
