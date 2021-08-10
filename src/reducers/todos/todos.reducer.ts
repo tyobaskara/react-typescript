d02a9b82a2a3488f792acbc7a1107c055f7c1802
Prasetya Aji Baskara <prasetya.baskara@btpn.com>
2021-08-10 11:00:56 +0700

    remove log

--- src/reducers/todos/todos.reducer.ts
+++ src/reducers/todos/todos.reducer.ts
@@ -14,7 +14,6 @@ export const initializer = (initialValue = initialState) =>
 export const TodosReducer = (state: todosState, action: actionType): todosState => {
   switch (action.type) {
     case todosTypes.ADD_TODO:
-      console.log('state', state);
       return [...state, action.payload]
     case todosTypes.REMOVE_TODO:
       return state.filter(state => state !== action.payload)
